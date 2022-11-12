31fec9abb70b3055d404cb2d3f326a93489c5805
mirror-kt <me@mirror-kt.dev>
2022-11-12 18:41:25 +0900

    refactor(ladle): omit unnecessary fragment

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -4,7 +4,5 @@ import { ChakraProvider } from "@chakra-ui/react";
 import type { GlobalProvider } from "@ladle/react";
 
 export const Provider: GlobalProvider = ({ children }) => (
-  <>
-    <ChakraProvider>{children}</ChakraProvider>
-  </>
+  <ChakraProvider>{children}</ChakraProvider>
 );
